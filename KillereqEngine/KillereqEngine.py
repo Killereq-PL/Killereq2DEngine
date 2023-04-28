5c42ac11793ac6be1881da1b017035f9b4af34cf
Killereq-PL <adam.kotek33@gmail.com>
2023-04-28 19:45:30 +0200

    Update KillereqEngine.py

--- KillereqEngine/KillereqEngine.py
+++ KillereqEngine/KillereqEngine.py
@@ -1,7 +1,15 @@
 import turtle
 
-class KillereqEngine:
-    def __init__(self, WindowSize: list):
-        self.sc = turtle.Screen()
-        self.sc.setup(WindowSize[0], WindowSize[1])
-        
+class KillereqEngine(turtle._Screen):
+    def __init__(self, WindowSize: tuple, BackgroundColor="white"):
+        self.tr = turtle.Turtle()
+        self.setup(WindowSize[0], WindowSize[1])
+        self.bgcolor(BackgroundColor)
+        self.mainloop()
+
+    def DrawImage(self, image, position):
+        pass
+
+if __name__ == "__main__":
+    Screen = KillereqEngine(WindowSize=(500, 500))
+    print("KillereqEngine")
