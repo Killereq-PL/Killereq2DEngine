99c52ed1ca28756679e0843d068aa6ee1b19cfcf
Killereq-PL <adam.kotek33@gmail.com>
2023-04-28 19:10:26 +0200

    update

--- KillereqEngine/KillereqEngine.py
+++ KillereqEngine/KillereqEngine.py
@@ -1,5 +1,7 @@
 import turtle
 
 class KillereqEngine:
-    def __init__(self):
-        pass
+    def __init__(self, WindowSize: list):
+        self.sc = turtle.Screen()
+        self.sc.setup(WindowSize[0], WindowSize[1])
+        
---
+++ KillereqEngine/__init__.py
@@ -0,0 +1,1 @@
+from KillereqEngine import KillereqEngine
